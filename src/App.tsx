e4ff8b2bd150e3a82b36c77210f98e16cd8db212
hiroto2002 <hiroto2002h14@icloud.com>
2023-01-18 02:21:32 +0900

    feat:テクスチャ貼り付けまで

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Scene } from '@babylonjs/core/scene'
 import SceneComponent from './lib/SceneComponent'
 import '@babylonjs/loaders' // これがないと読み込めない
 import { Engine } from '@babylonjs/core/Engines/engine'
+import { Vector3 } from '@babylonjs/core/Maths/math'
 
 const onSceneReady = (scene: Scene) => {
   // カメラの作成, 回転
@@ -26,15 +27,25 @@ const onSceneReady = (scene: Scene) => {
   light.intensity = 1.0
 
   // SoccerBall.babylonからノードの読み込み
-  const assetsManager = new BABYLON.AssetsManager(scene)
-  const iPhone = assetsManager.addMeshTask('task', '', '/assets/', 'iPhone.glb')
+  BABYLON.SceneLoader.ImportMeshAsync('', '/assets/', 's7.obj').then(
+    (result) => {
+      // 読み込みが完了したら
+      const back = scene.getMeshByName('iphone_6s_low_poly:polySurface14')
+      // house1.position.y = 2; // y 軸方向(↑)に +2
+      console.log(back)
+      const roofMat = new BABYLON.StandardMaterial('roofMat')
+      roofMat.diffuseTexture = new BABYLON.Texture(
+        '/assets/tpl_avocado.png',
+        scene,
+      )
+      if (back) {
+        back.material = roofMat
+      }
+    },
+  )
+  // const assetsManager = new BABYLON.AssetsManager(scene)
+  // assetsManager.addMeshTask('task', '', '/assets/', 's7.obj')
   // assetsManager.addMeshTask("task", "SoccerBall", iPhoneAsset);
-
-  assetsManager.load()
-  // BABYLON.SceneLoader.ImportMeshAsync("", "/assets/","scene.babylon");
-
-  // //Begin animation - object to animate, first frame, last frame and loop if true
-  // // アニメーションを開始する
 }
 
 export default function App() {
